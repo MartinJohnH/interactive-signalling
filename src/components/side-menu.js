f17ce5264eecd76fb26ba51d9289e012ee09f50e
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-12-10 12:37:01 -0500

    changed link text

--- src/components/side-menu.js
+++ src/components/side-menu.js
@@ -9,7 +9,7 @@ const SideMenu = () => (
         <li><AniLink className="link" paintDrip to="/about/" hex="#08e192" duration={0.8}>About</AniLink></li>
         <li><AniLink className="link" paintDrip to="/process/" hex="#08e192" duration={0.8}>Process</AniLink></li>
         <li><AniLink className="link" paintDrip to="/team/" hex="#08e192" duration={0.8}>Team</AniLink></li>
-        <a className="connect-button" href="https://martinjohnh.github.io/interactive-signalling/" target="_blank" rel="noopener noreferrer">Visit</a>
+        <a className="connect-button" href="https://martinjohnh.github.io/interactive-signalling/" target="_blank" rel="noopener noreferrer">View</a>
     </ul>
   </menu>
 )
